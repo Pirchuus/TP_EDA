e7234559bad45793880ad2172724c8496c1bad24
Pirchuus <diogopinheiropinhas@gmail.com>
2023-03-03 18:58:31 +0000

    Header Files

--- TP_EDA/cliente.h
+++ TP_EDA/cliente.h
@@ -4,7 +4,7 @@
 // Estrutura para representar um cliente
 typedef struct cliente
 {
-    int id; // Identificador único do cliente
+    int nif; // Identificador único do cliente
     char nome[50]; // Nome do cliente
     float saldo; // Saldo disponível na carteira do cliente
     struct mobilidade* mobilidade_alugada; // Ponteiro para o meio de mobilidade elétrica alugado pelo cliente (ou NULL se não estiver alugando nenhum)
@@ -13,8 +13,8 @@ typedef struct cliente
 } Cliente;
 
 void listarClientes(Cliente* cliente);
-int existeCliente(Cliente* cliente, int id);
-Cliente* inserirCliente(Cliente* cliente, int id, char nome[], float saldo);
-Cliente* removerCliente(Cliente* cliente, int id);
-Cliente* atualizarCliente(Cliente* cliente, int id, char nome[], float saldo);
+int existeCliente(Cliente* cliente, int nif);
+Cliente* inserirCliente(Cliente* cliente, int nif, char nome[], float saldo);
+Cliente* removerCliente(Cliente* cliente, int nif);
+Cliente* atualizarCliente(Cliente* cliente, int nif, char nome[], float saldo);
 
